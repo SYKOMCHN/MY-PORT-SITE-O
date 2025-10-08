018d790817a4bb7c29d996d2ffc8e7a64d7193f3
Oscar <highspeedtumbleweed@gmail.com>
2025-10-07 22:21:23 -0400

    need to fix contact button, or every button. OH DEAR LAWD DX

--- src/js_files/index.js
+++ src/js_files/index.js
@@ -1,4 +1,4 @@
-console.log("index.js loaded");
+console.log("index loaded");
 
 let pageWidth = window.innerWidth;
 let pageHeight = window.innerWidth;
@@ -6,11 +6,11 @@ let pageHeight = window.innerWidth;
 //const container = document.getElementById("container");
 //window.container = container;
 
-const pageContainer = window.document.createElement("div");
+const pageContainer = document.createElement("div");
 window.pageContainer = pageContainer; 
 pageContainer.style.backgroundColor = "rgb(37, 53, 73)";
 pageContainer.style.borderRadius = "10px";
-pageContainer.style.height = (pageHeight - 18) + "px";
+pageContainer.style.height = (window.innerHeight - 18) + "px";
 //pageContainer.style.display = "flex";
 window.container.appendChild(pageContainer);
 
@@ -20,27 +20,23 @@ window.page = page;
 page.style.backgroundColor = "rgb(252, 250, 244)";
 page.style.borderRadius = "10px";
 page.style.align = "right";
-page.style.width = (pageWidth - 145) + "px ";
-page.style.height = (pageHeight - 47) + "px";
+page.style.width = (window.innerWidth - 145) + "px ";
+page.style.height = (window.innerHeight - 47) + "px";
 page.style.marginTop = "14px";
 page.style.overflow = "hidden";
 page.style.float = "right";
 window.pageContainer.appendChild(page);
 
-/* mess around a bit
+
 function resizePage() {
     pageWidth = window.innerWidth;
     pageHeight = window.innerHeight;
     pageContainer.style.height = (pageHeight - 18) + "px";
     page.style.width = (pageWidth - 145) + "px ";
     page.style.height = (pageHeight - 47) + "px";
-    navigationTab.style.width = ((pageWidth / pageWidth) * 120 + "px");
-    navigationTab.style.height = (pageHeight - 118) + "px";
-    naviContainer.style.width = ((pageWidth / pageWidth) * 90 + "px");
-    naviContainer.style.height = (pageHeight - 144) + "px";
 }
 
 window.addEventListener('resize', resizePage);
-*/
+
 
 
--- src/js_files/nav_tab.js
+++ src/js_files/nav_tab.js
@@ -1,5 +1,5 @@
-console.log("nav_tab.js loaded");
-//navigation tab setup ------------------------------------------
+console.log("nav_tab loaded");
+//navigation tab setup ------------------------------------------------
 const navigationTab = document.createElement("div");
 window.navigationTab = navigationTab;
 navigationTab.style.paddingTop= "50px";
@@ -38,7 +38,7 @@ else{
     homePage.addEventListener('mouseover', function() {
         homePage.textContent = "Home";
         homePage.style.display = "flex";
-        homePage.style.backgroundColor = "rgba(163, 177, 82, 1)";
+        homePage.style.backgroundColor = "rgba(241, 76, 34, 1)";
         homePage.style.border = "none";
         homePage.style.padding ="20px 30px";
         homePage.style.float = "right";
@@ -57,12 +57,13 @@ else{
 }
 
 homePage.addEventListener('click', function() {
-    window.location.href = "index.html";
+    window.location.href = "../../index.html";
 });
 
 window.naviContainer.appendChild(homePage);
 
 //project page button setup ----------------------------------------
+
 const projectPage = document.createElement("button");   
 window.projectPage = projectPage;
 projectPage.textContent = "Projects";
@@ -71,7 +72,7 @@ projectPage.style.border = "none";
 projectPage.style.padding ="20px 10px";
 projectPage.style.float = "right";
 
-if(window.location.href.indexOf("src/pages/authors_projects_page.html") > -1) {
+if(window.location.href.indexOf("src/pages/projects_page.html" || "projects_page.html") > -1) {
     console.log("on project page");
     projectPage.style.cursor = "pointer";
     projectPage.style.backgroundColor = "rgb(252, 250, 244)";
@@ -104,24 +105,172 @@ else{
 }
 
 projectPage.addEventListener('click', function() {
-    window.location.href = "src/pages/projects_page.html";
+      if (window.location.href.indexOf("src/pages") > -1){
+        window.location.href = "projects_page.html";
+    }
+    else{
+        window.location.href = "src/pages/projects_page.html";
+    }
 });
 
 window.naviContainer.appendChild(projectPage);
+
 //artwork page button setup ---------------------------------------
-/*
-const artworkPage = document.createElement("button");   
+
+const artworkPage = document.createElement("button");  
+window.artworkPage = artworkPage;
 artworkPage.textContent = "Artworks";
-navigationTab.appendChild(artworkPage);
+artworkPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+artworkPage.style.border = "none";
+artworkPage.style.padding ="20px 10px";
+artworkPage.style.float = "right";
+
+if(window.location.href.indexOf("src/pages/artworks_page.html" || "artworks_page.html") > -1) {
+    console.log("on artwork page");
+    artworkPage.style.cursor = "pointer";
+    artworkPage.style.backgroundColor = "rgb(252, 250, 244)";
+    artworkPage.style.border = "none";
+    artworkPage.style.padding ="20px 30px";
+    artworkPage.style.float = "right";
+    artworkPage.style.marginRight = "-40px";
+}
+else{
+    console.log("not on artwork page");
+    artworkPage.addEventListener('mouseover', function() {
+        artworkPage.textContent = "Artworks";
+        artworkPage.style.display = "flex";
+        artworkPage.style.backgroundColor = "rgba(241, 76, 34, 1)";
+        artworkPage.style.border = "none";
+        artworkPage.style.padding ="20px 30px";
+        artworkPage.style.float = "right";
+        artworkPage.style.marginRight = "-20px";
+    });
+
+    artworkPage.addEventListener('mouseout', function() {
+       artworkPage.style.cursor = "pointer";
+       artworkPage.textContent = "Artworks";
+       artworkPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+       artworkPage.style.border = "none";
+       artworkPage.style.padding ="20px 10px";
+       artworkPage.style.float = "right";
+       artworkPage.style.marginRight = "0px";
+    });
+}
+
+artworkPage.addEventListener('click', function() {
+    if (window.location.href.indexOf("src/pages") > -1){
+        window.location.href = "artworks_page.html";
+    }
+    else{
+        window.location.href = "src/pages/artworks_page.html";
+    }
+});
+
+window.naviContainer.appendChild(artworkPage);
 
 const aboutPage = document.createElement("button");   
-aboutPage.textContent = "About Me";
-navigationTab.appendChild(aboutPage);
+window.aboutPage = aboutPage;
+aboutPage.textContent = "About";
+aboutPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+aboutPage.style.border = "none";
+aboutPage.style.padding ="20px 10px";
+aboutPage.style.float = "right";
+
+if(window.location.href.indexOf("src/pages/aboutme_page.html" || "aboutme_page.html") > -1) {
+    console.log("on about me page");
+    aboutPage.style.cursor = "pointer";
+    aboutPage.style.backgroundColor = "rgb(252, 250, 244)";
+    aboutPage.style.border = "none";
+    aboutPage.style.padding ="20px 30px";
+    aboutPage.style.float = "right";
+    aboutPage.style.marginRight = "-40px";
+}
+else{
+    console.log("not on artwork page");
+    aboutPage.addEventListener('mouseover', function() {
+        aboutPage.textContent = "About";
+        aboutPage.style.display = "flex";
+        aboutPage.style.backgroundColor = "rgba(241, 76, 34, 1)";
+        aboutPage.style.border = "none";
+        aboutPage.style.padding ="20px 30px";
+        aboutPage.style.float = "right";
+        aboutPage.style.marginRight = "-20px";
+    });
+
+    aboutPage.addEventListener('mouseout', function() {
+       aboutPage.style.cursor = "pointer";
+       aboutPage.textContent = "About";
+       aboutPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+       aboutPage.style.border = "none";
+       aboutPage.style.padding ="20px 10px";
+       aboutPage.style.float = "right";
+       aboutPage.style.marginRight = "0px";
+    });
+}
+
+aboutPage.addEventListener('click', function() {
+    if (window.location.href.indexOf("src/pages") > -1){
+        window.location.href = "aboutme_page.html";
+    }
+    else{
+        window.location.href = "src/pages/aboutme_page.html";
+    }
+});
+
+window.naviContainer.appendChild(aboutPage);
 
 const contactPage = document.createElement("button");   
+window.contactPage = contactPage;
 contactPage.textContent = "Contact";
-navigationTab.appendChild(contactPage);
+contactPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+contactPage.style.border = "none";
+contactPage.style.padding ="20px 10px";
+contactPage.style.float = "right";
 
+if(window.location.href.indexOf("src/pages/contact_page.html" || "contact_page.html") > -1) {
+    console.log("on contact page");
+    contactPage.style.cursor = "pointer";
+    contactPage.style.backgroundColor = "rgb(252, 250, 244)";
+    contactPage.style.border = "none";
+    contactPage.style.padding ="20px 30px";
+    contactPage.style.float = "right";
+    aboutPage.style.marginRight = "-40px";
+}
+else{
+    console.log("not on contact page");
+    contactPage.addEventListener('mouseover', function() {
+        contactPage.textContent = "Contact";
+        contactPage.style.display = "flex";
+        contactPage.style.backgroundColor = "rgba(241, 76, 34, 1)";
+        contactPage.style.border = "none";
+        contactPage.style.padding ="20px 30px";
+        contactPage.style.float = "right";
+        contactPage.style.marginRight = "-20px";
+    });
+
+    contactPage.addEventListener('mouseout', function() {
+       aboutPage.style.cursor = "pointer";
+       aboutPage.textContent = "Contact";
+       aboutPage.style.backgroundColor = "rgba(149, 177, 209, 1)";
+       aboutPage.style.border = "none";
+       aboutPage.style.padding ="20px 10px";
+       aboutPage.style.float = "right";
+       aboutPage.style.marginRight = "0px";
+    });
+}
+
+contactPage.addEventListener('click', function() {
+    if (window.location.href.indexOf("src/pages") > -1){
+        window.location.href = "contact_page.html";
+    }
+    else{
+        window.location.href = "src/pages/contact_page.html";
+    }
+});
+
+window.naviContainer.appendChild(contactPage);
+
+/*
 const shortcutPage = document.createElement("button");   
 shortcutPage.textContent = "Shortcuts";
 navigationTab.appendChild(shortcutPage);    
@@ -130,3 +279,11 @@ navigationTab.appendChild(shortcutPage);
 window.pageContainer.appendChild(navigationTab);
 
 //probably could have made a class for this :/
+
+function resizeNavi() {
+    navigationTab.style.height = (pageHeight - 118) + "px";
+    navigationTab.style.width = ((pageWidth / pageWidth) * 120 + "px");
+    naviContainer.style.height = (pageHeight - 144) + "px";
+    naviContainer.style.width = ((pageWidth / pageWidth) * 90 + "px");
+}
+window.addEventListener('resize', resizeNavi);
